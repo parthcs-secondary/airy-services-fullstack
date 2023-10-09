92d0e74720dd3464341d9a15b9cf0d2441d88866
parthcs-secondary <parthcs626@gmail.com>
2023-10-09 22:33:16 +0530

    made changes in shell script: 4

--- dockerbuild-automated.sh
+++ dockerbuild-automated.sh
@@ -1,10 +1,16 @@
 echo "Build script started"
 
-docker build -t e-commerce-backend-api-gateway-v2 -f api-gateway/Dockerfile
-docker build -t e-commerce-backend-authorization-service-v2 -f authorization-service/Dockerfile
-docker build -t e-commerce-backend-inventory-service-v2 -f inventory-service/Dockerfile
-docker build -t e-commerce-backend-order-service-v2 -f order-service/Dockerfile
-docker build -t e-commerce-backend-product-service-v2 -f product-service/Dockerfile
-docker build -t e-commerce-backend-service-registry-v2 -f service-registry/Dockerfile
+docker build -t e-commerce-backend-api-gateway-v2 -f api-gateway/Dockerfile ./api-gateway/.
+docker build -t e-commerce-backend-authorization-service-v2 -f authorization-service/Dockerfile ./authorization-service/. 
+docker build -t e-commerce-backend-inventory-service-v2 -f inventory-service/Dockerfile ./inventory-service/.
+docker build -t e-commerce-backend-order-service-v2 -f order-service/Dockerfile ./order-service/.
+docker build -t e-commerce-backend-product-service-v2 -f product-service/Dockerfile ./product-service/.
+docker build -t e-commerce-backend-service-registry-v2 -f service-registry/Dockerfile ./service-registry/.
 
-echo "Build completed. Please check images with cmd 'docker images'"
+
+if [[ $? -eq 0 ]]; then
+        echo "Build completed. Please check images with cmd 'docker images'"
+
+else
+        echo "Failed to build image"
+fi
