3c6da70084a4a7eaf87aed1b57080ab67fe6809a
parthcs-secondary <parthcs626@gmail.com>
2023-10-09 23:09:39 +0530

    changed image name in script

--- dockerbuild-automated.sh
+++ dockerbuild-automated.sh
@@ -1,11 +1,11 @@
 echo "Build script started"
 
-docker build -t e-commerce-backend-api-gateway-v2 -f api-gateway/Dockerfile ./api-gateway/.
-docker build -t e-commerce-backend-authorization-service-v2 -f authorization-service/Dockerfile ./authorization-service/. 
-docker build -t e-commerce-backend-inventory-service-v2 -f inventory-service/Dockerfile ./inventory-service/.
-docker build -t e-commerce-backend-order-service-v2 -f order-service/Dockerfile ./order-service/.
-docker build -t e-commerce-backend-product-service-v2 -f product-service/Dockerfile ./product-service/.
-docker build -t e-commerce-backend-service-registry-v2 -f service-registry/Dockerfile ./service-registry/.
+docker build -t parthkarad/e-commerce-backend-api-gateway-v2 -f api-gateway/Dockerfile ./api-gateway/.
+docker build -t parthkarad/e-commerce-backend-authorization-service-v2 -f authorization-service/Dockerfile ./authorization-service/. 
+docker build -t parthkarad/e-commerce-backend-inventory-service-v2 -f inventory-service/Dockerfile ./inventory-service/.
+docker build -t parthkarad/e-commerce-backend-order-service-v2 -f order-service/Dockerfile ./order-service/.
+docker build -t parthkarad/e-commerce-backend-product-service-v2 -f product-service/Dockerfile ./product-service/.
+docker build -t parthkarad/e-commerce-backend-service-registry-v2 -f service-registry/Dockerfile ./service-registry/.
 
 
 if [ $? -eq 0 ]; then
