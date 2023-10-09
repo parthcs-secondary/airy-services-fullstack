7a640de43d01b958e69f44486ec687e5a84ea220
parthcs-secondary <parthcs626@gmail.com>
2023-10-09 21:10:44 +0530

    made changes in shell script

--- dockerbuild-automated.sh
+++ dockerbuild-automated.sh
@@ -1,10 +1,10 @@
 echo "Build script started"
 
-sh "Docker build -f api-gateway/Dockerfile -t e-commerce-backend-api-gateway-v2"
-sh "Docker build -f authorization-service/Dockerfile -t e-commerce-backend-authorization-service-v2"
-sh "Docker build -f inventory-service/Dockerfile -t e-commerce-backend-inventory-service-v2"
-sh "Docker build -f order-service/Dockerfile -t e-commerce-backend-order-service-v2"
-sh "Docker build -f product-service/Dockerfile -t e-commerce-backend-product-service-v2"
-sh "Docker build -f service-registry/Dockerfile -t e-commerce-backend-service-registry-v2"
+Docker build -f api-gateway/Dockerfile -t e-commerce-backend-api-gateway-v2
+Docker build -f authorization-service/Dockerfile -t e-commerce-backend-authorization-service-v2
+Docker build -f inventory-service/Dockerfile -t e-commerce-backend-inventory-service-v2
+Docker build -f order-service/Dockerfile -t e-commerce-backend-order-service-v2
+Docker build -f product-service/Dockerfile -t e-commerce-backend-product-service-v2
+Docker build -f service-registry/Dockerfile -t e-commerce-backend-service-registry-v2
 
 echo "Build completed. Please check images with cmd 'docker images'"
